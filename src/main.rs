6e9fa798406971d251f7cf89c1c7cbfce7ec3c6b
Aceeri <conmcclusk@gmail.com>
2025-10-14 19:42:04 -0700

    Turn off vsync

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use bevy::prelude::*;
+use bevy::window::PresentMode;
 use bevy_inspector_egui::bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() -> AppExit {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // present_mode: PresentMode::AutoNoVsync,
+                present_mode: PresentMode::Immediate,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_plugins(EguiPlugin::default())
         .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(arch::core::shared)
